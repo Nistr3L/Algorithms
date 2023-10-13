ad8e39a38917df82ba982e12c13faaf63180b8a0
Nistr3L <96323232+Nistr3L@users.noreply.github.com>
2023-10-14 02:31:38 +1100

    Add files via upload

---
+++ gcd.py
@@ -0,0 +1,32 @@
+"""
+The division-based implementation of Euclidean's algorithm
+for calculating the Greatest Common Divisor
+"""
+
+def gcd(a: int, b: int) -> int:
+    while b != 0:
+        t = b
+        b = a % b
+        a = t
+    return abs(a)
+
+
+def main():
+    try:
+        nums = input("Enter two integers separated by comma (,): ").split(",")
+        num_1 = int(nums[0])
+        num_2 = int(nums[1])
+        print(
+            f"gcd({num_1}, {num_2}) = "
+            f"{gcd(num_1, num_2)}"
+        )
+        
+    except (IndexError, UnboundLocalError, ValueError):
+        print("Wrong input")
+
+
+if __name__ == "__main__":
+    main()
+
+
+
