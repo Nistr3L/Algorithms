7687c6ade4fc52fbfd28c0676a5c1d9eb00ae555
Nistr3L <96323232+Nistr3L@users.noreply.github.com>
2023-10-16 22:17:43 +1100

    Update greatest_common_divisor.py

--- greatest_common_divisor.py
+++ greatest_common_divisor.py
@@ -1,17 +1,16 @@
-def gcd(a: int, b: int) -> int:    
-    while b != 0:               
-        t = b                   
-        b = a % b               
-        a = t                   
-    return abs(a)
-
+def gcd(a, b):
+    if b == 0:
+        return abs(a)
+    else:
+        return abs(gcd(b, a % b))
 
 def main():
-    print("The division-based implementation of Euclidean's algorithm\nfor calculating the Greatest Common Divisor.\n")
+    print("The recursive-based implementation of Euclidean's algorithm\nfor calculating the Greatest Common Divisor.\n")
     try:
         nums = input("Enter two integers separated by comma (,): ").split(",")
         num_1 = int(nums[0])
         num_2 = int(nums[1])
+
         print(
             f"gcd({num_1}, {num_2}) = "
             f"{gcd(num_1, num_2)}"
